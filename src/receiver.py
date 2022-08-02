898c8b7ec45f55676fd701cc137d22d0d12def54
Nils Rother <NilsRother123@gmail.com>
2022-08-02 20:23:44 +0200

    Add compression

--- src/receiver.py
+++ src/receiver.py
@@ -1,13 +1,21 @@
+import time
 import cv2
 import imagezmq
 
 hub = imagezmq.ImageHub()
-
+prev_tstamp = 0
+new_tstamp = 0
 while True:
     _, image = hub.recv_image()
+    image = cv2.imdecode(image, 1)
+    new_tstamp = time.time()
+    fps = 1 / (new_tstamp - prev_tstamp)
+    prev_tstamp = new_tstamp
+    image = cv2.putText(image, str(fps), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2, cv2.LINE_AA)
     cv2.namedWindow('receiver', cv2.WINDOW_NORMAL)
     cv2.imshow("receiver", image)
     k = cv2.waitKey(1)
     if k == 27:
         break
     hub.send_reply(b'OK')
+
--- src/sender.py
+++ src/sender.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import socket
 
+import cv2
 from pypylon import pylon
 import imagezmq
 
@@ -10,6 +11,7 @@ parser.add_argument('host')
 parser.add_argument('-x', '--width', type=int, default=1920)
 parser.add_argument('-y', '--height', type=int, default=1080)
 parser.add_argument('-e', '--exposure', type=int, default=33000)
+parser.add_argument('-c', '--compression', type=int, default=95)
 args = parser.parse_args()
 
 sender = imagezmq.ImageSender(connect_to=args.host)
@@ -33,6 +35,8 @@ converter = pylon.ImageFormatConverter()
 converter.OutputPixelFormat = pylon.PixelType_BGR8packed
 converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), args.compression]
+
 while camera.IsGrabbing():
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
@@ -40,6 +44,8 @@ while camera.IsGrabbing():
         # Access the image data
         image = converter.Convert(grabResult)
         img = image.GetArray()
+        img = cv2.imgencode('.jpg', img, encode_param)
+
         sender.send_image(sender_name, img)
     grabResult.Release()
 
